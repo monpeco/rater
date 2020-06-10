edf5264684c85941d17ea303ef354ff2c5a9f3b6
monpeco <monpeco@gmail.com>
2020-06-10 19:11:48 +0200

    Add extra messages to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-	cout << "program's end 123";
+	cout << "this program is starting" << endl;
+	int tem = 5;
+	cout << tem << endl;
+	cout << "program completed";
 	return 0;
 }
